3693a31a068128b62d86e0d414540d3246a20232
Nathan Witmer <nwitmer@gmail.com>
2008-05-31 18:17:28 -0700

    Refactored gitjour command and application to use Ara's main gem
    * Semantics of command line remains (mostly) unchanged
    * More help is available via command-line now

--- lib/gitjour/application.rb
+++ lib/gitjour/application.rb
@@ -1,72 +1,101 @@
 require 'rubygems'
 require 'dnssd'
+require 'main'
 require 'set'
 Thread.abort_on_exception = true
 
 module Gitjour
   GitService = Struct.new(:name, :host, :description)
-  class Application
 
-    class << self
-      def run(operation = nil, *arguments)
-        case operation
-          when "list"
-            list
-          when "clone"
-            clone(*arguments)
-          when "serve"
-            serve(*arguments)
-          when "remote"
-            remote(*arguments)
-          else
-            help
-        end
-      end
+  Application = Main.create do
 
-      private
-			def list
-				service_list.each do |service|
+    run { help! }
+
+    description "Serve up and use git repositories via Bonjour/DNSSD."
+
+    examples <<-TXT
+      gitjour help
+      gitjour help <command>
+    TXT
+
+    mode "list" do
+      description "list the available repositories shared via Bonjour"
+
+      run do
+        service_list.each do |service|
           puts "=== #{service.name} on #{service.host} ==="
           puts "  gitjour clone #{service.name}"
           puts "  #{service.description}" if service.description && service.description != '' && service.description !~ /^Unnamed repository/
           puts
         end
-			end
+      end
+    end
+
+    mode "clone" do
+      description "clone a repository via gitjour"
+
+      argument "repository_name" do
+        required
+        attribute # make it available in local scope
+      end
+
+      run do
+        host, name_of_share = get_host_and_share(repository_name)
+        system("git clone git://#{host}/ #{name_of_share}/")
+      end
+    end
+
+    mode "serve" do
+      description "Serve a git repository via Bonjour"
+
+      argument "path" do
+        description "The directory containing one or more git repositories to serve"
+        default '.'
+      end
+
+      option "n", "name" do
+        description "an alternate name to share the repository as (only works when serving a single repo)"
+        argument_optional
+        attribute "share_name"
+      end
+
+      run do
+        path = File.expand_path(params["path"].value)
+        if File.exists?("#{path}/.git")
+          announce_repo(path, share_name)
+        else
+          Dir["#{path}/*"].each{|dir| announce_repo(dir) if File.directory?(dir)}
+        end
+        `git-daemon --verbose --export-all --base-path=#{path} --base-path-relaxed`
+      end
+    end
+
+    mode "remote" do
+      description "Add a gitjour remote into your current repository"
+
+      argument "repository_name" do
+        description "The repository to add as a remote"
+        required
+        attribute
+      end
+
+      run do
+        host, name_of_share = get_host_and_share(repository_name)
+        system("git remote add #{name_of_share} git://#{host}/")
+      end
+    end
+
+  end
+
+  class Application
+
+    private
 
       def get_host_and_share(repository_name)
         name_of_share = repository_name || fail("You have to pass in a name")
         host = service_list(name_of_share).detect{|service| service.name == name_of_share}.host rescue exit_with!("Couldn't find #{name_of_share}")
-        system("git clone git://#{host}/ #{name_of_share}/")  
-        [host, name_of_share]
-      end
-
-      def clone(repository_name,*rest)
-        host, name_of_share = get_host_and_share(repository_name)
         system("git clone git://#{host}/ #{name_of_share}/")
-      end
-
-      def remote(repository_name,*rest)
-        host, name_of_share = get_host_and_share(repository_name)
-        system("git remote add #{name_of_share} git://#{host}/")
-      end
-
-      def serve(path, share_name = nil, *rest)
-        path ||= Dir.pwd
-        path = File.expand_path(path)
-        File.exists?("#{path}/.git") ? announce_repo(path, share_name) : Dir["#{path}/*"].each{|dir| announce_repo(dir) if File.directory?(dir)}
-        `git-daemon --verbose --export-all --base-path=#{path} --base-path-relaxed`
-      end
-
-      def help
-        puts "Serve up and use git repositories via Bonjour/DNSSD."
-        puts "Usage: gitjour <command> [args]"
-        puts
-        puts "  list      Lists available repositories."
-        puts "  clone     Clone a gitjour served repository."
-        puts "  serve     Serve up the current directory via gitjour."
-        puts "  remote    Add a Bonjour remote into your current repository."
-        puts "            Optionally pass name to not use pwd."
-        puts
+        [host, name_of_share]
       end
 
       def exit_with!(message)
@@ -88,7 +117,7 @@ module Gitjour
             end
           end
         end
-        puts "Gathering for up to #{wait_seconds} seconds..."
+        puts "Searching for repositories for up to #{wait_seconds} seconds..."
         waiting_thread.join
         service.stop
         service_list
@@ -103,7 +132,7 @@ module Gitjour
           puts "Registered #{name}.  Starting service."
         end
       end
-    end
+
   end
 end
 
